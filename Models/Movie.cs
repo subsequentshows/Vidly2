3b01ae08d5b766a308fad30f01883c17a8f362f1
VuHuuVan <van.vuhuu@euroland.com>
2023-06-27 17:32:01 +0700

    update 2

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly2.Models
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Movie_Name { get; set; }
+    public int Movie_Description { get; set; }
+  }
+}
